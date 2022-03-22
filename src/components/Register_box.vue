6094897d3cc72567e40b356194b4c48ee2c186a0
boski <boskitechh@gmail.com>
2022-03-22 22:24:34 +0100

    dashboard frontend cont.

--- src/components/Register_box.vue
+++ src/components/Register_box.vue
@@ -1,23 +1,25 @@
 <template>
-    <v-container width="100%" height="100%" class="my-12">
+    <v-container width="100%" height="100%" class="my-8">
         <v-col>
             <v-row justify="center" >
-                <v-sheet color="white" elevation="4" width="600px" height="480px" class="my-12">
+                <v-sheet color="white" elevation="4" width="600px" height="auto" class="my-8">
                     <v-toolbar flat color="#222222" class="white--text">
                         <v-toolbar-title>Register</v-toolbar-title>
                     </v-toolbar>
                     <v-sheet class="pa-10">
                         <v-row>
-                            <v-text-field outlined label="Firstname"></v-text-field><v-spacer></v-spacer>
-                            <v-text-field outlined label="Lastname"></v-text-field>
-                        </v-row>
-                        <v-row>
-                            <v-text-field outlined label="Email"></v-text-field><v-spacer></v-spacer>
-                            <v-text-field outlined label="Username"></v-text-field>
-                        </v-row>
-                        <v-row>
-                            <v-text-field outlined label="Password"></v-text-field><v-spacer></v-spacer>
-                            <v-text-field outlined label="Confirm Password"></v-text-field>
+                            <v-col cols="6">
+                                <v-text-field outlined label="Firstname"></v-text-field>
+                                <v-text-field outlined label="Email"></v-text-field><v-spacer></v-spacer>
+                                <v-text-field outlined label="Country"></v-text-field>
+                                <v-text-field outlined label="Password"></v-text-field><v-spacer></v-spacer>
+                            </v-col>
+                            <v-col cols="6">
+                                <v-text-field outlined label="Lastname"></v-text-field>
+                                <v-text-field outlined label="Username"></v-text-field>
+                                <v-select outlined label="Gender"></v-select>
+                                <v-text-field outlined label="Confirm Password"></v-text-field>
+                            </v-col>
                         </v-row>
                         <v-row>
                         <v-btn color="orange" width="100%" height="50px" class="white--text h3--text" style="font-size:19px" >Register</v-btn>
--- src/views/Manage_products.vue
+++ src/views/Manage_products.vue
@@ -1,30 +1,194 @@
 <template>
     <div class="my-3">
-        <h2 >Manage Products</h2>
-        <v-row>
-            <v-col cols="5">
-                
+        <v-row class="pa-0">
+            <v-col cols="7">
+            <h2>Manage Product</h2>
             </v-col>
+            <v-col cols="2">
+                <v-text-field :items="items" label="Search" ></v-text-field>
+            </v-col>
+            <v-col cols="2">
+                <v-select :items="items" label="Filter By"  ></v-select>
+            </v-col>
+            
+        </v-row>
+        <v-row>
+            <v-col cols="11">
+            <table class="styled-table">
+                <thead>
+                    <tr>
+                        <th>S/No</th>
+                        <th>Product ID</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Brand.</th>
+                        <th>Category</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>12</td>
+                        <td>156267278</td>
+                        <td>Lip Balm</td>
+                        <td>12090</td>
+                        <td>234</td>
+                        <td>Chanel</td>
+                        <td>Lips</td>
+                         <td>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn fab small color="green white--text" v-bind="attrs" v-on="on" >
+                                    <v-icon>mdi-plus</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Add Product</span>
+                        </v-tooltip>
+                        </td>
+                        <td>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn fab small color="orange white--text" v-bind="attrs" v-on="on" >
+                                    <v-icon>mdi-pencil</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Edit</span>
+                        </v-tooltip>
+                        </td>
+                        <td>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn fab small color="red white--text" v-bind="attrs" v-on="on" >
+                                    <v-icon>mdi-close</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Remove Product</span>
+                        </v-tooltip>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>12</td>
+                        <td>156267278</td>
+                        <td>Lip Balm</td>
+                        <td>12090</td>
+                        <td>234</td>
+                        <td>Chanel</td>
+                        <td>Lips</td>
+                         <td>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn fab small color="green white--text" v-bind="attrs" v-on="on" >
+                                    <v-icon>mdi-plus</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Add Product</span>
+                        </v-tooltip>
+                        </td>
+                        <td>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn fab small color="orange white--text" v-bind="attrs" v-on="on" >
+                                    <v-icon>mdi-pencil</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Edit</span>
+                        </v-tooltip>
+                        </td>
+                        <td>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn fab small color="red white--text" v-bind="attrs" v-on="on" >
+                                    <v-icon>mdi-close</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Remove Product</span>
+                        </v-tooltip>
+                        </td>
+                    </tr>
+                      <tr>
+                        <td>12</td>
+                        <td>156267278</td>
+                        <td>Lip Balm</td>
+                        <td>12090</td>
+                        <td>234</td>
+                        <td>Chanel</td>
+                        <td>Lips</td>
+                         <td>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn fab small color="green white--text" v-bind="attrs" v-on="on" >
+                                    <v-icon>mdi-plus</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Add Product</span>
+                        </v-tooltip>
+                        </td>
+                        <td>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn fab small color="orange white--text" v-bind="attrs" v-on="on" >
+                                    <v-icon>mdi-pencil</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Edit</span>
+                        </v-tooltip>
+                        </td>
+                        <td>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn fab small color="red white--text" v-bind="attrs" v-on="on" >
+                                    <v-icon>mdi-close</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Remove Product</span>
+                        </v-tooltip>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </v-col>
         </v-row>
     </div>
 </template>
 
-
 <script>
 export default {
-    name:"ManageProducts"
+    name: "ManageProducts",
 }
 </script>
 
 <style scoped>
-.mainbox{
-        width: 100%;
-        height: auto;
-        margin: 30px 0;
-        margin-bottom: 250px;
-        /*box-shadow: 0 0px 8px rgb(0 0 0 / 0.2);*/
-        overflow: none;
-        float: left;
-        border: 1px solid #bfbfbf;
-    }
+.styled-table {
+    border-collapse: collapse;
+    margin: 5px 0;
+    font-size: 14px;
+    font-family: sans-serif;
+    width: 100%;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.15);
+}
+
+.styled-table thead tr {
+    background-color: #333333;
+    color: #ffffff;
+    text-align: left;
+}
+
+.styled-table th, .styled-table td {
+    padding: 12px 15px;
+}
+
+.styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+    border-bottom: 2px solid #333333;
+}
 </style>
